f4c3fe8cebc149107eec56f21abc2e499ac5af77
Mark Alexander <6917497+m15e@users.noreply.github.com>
2020-09-04 20:11:05 +0800

    Controller setup

---
+++ app/models/event.rb
@@ -0,0 +1,4 @@
+class Event < ApplicationRecord
+  belongs_to :host, class_name: "User"
+  has_many :guests, class_name: "User"
+end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ApplicationRecord
+  has_many :host_events, foreign_key: 'host_id', class_name: 'Event'
+  has_many :guest_events, foreign_key: 'guest_id', class_name: 'Event'
+end
